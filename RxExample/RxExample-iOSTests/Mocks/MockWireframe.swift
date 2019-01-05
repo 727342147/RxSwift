340a98b08fc9edaa771d2b73a5ef246463ee9bdb
freak4pc <freak4pc@gmail.com>
2019-01-05 18:18:44 +0200

    void_retun: Prefer Void vs. () in closure return types

--- RxExample/RxExample-iOSTests/Mocks/MockWireframe.swift
+++ RxExample/RxExample-iOSTests/Mocks/MockWireframe.swift
@@ -10,10 +10,10 @@ import RxSwift
 import struct Foundation.URL
 
 class MockWireframe: Wireframe {
-    let _openURL: (URL) -> ()
+    let _openURL: (URL) -> Void
     let _promptFor: (String, Any, [Any]) -> Observable<Any>
 
-    init(openURL: @escaping (URL) -> () = notImplementedSync(),
+    init(openURL: @escaping (URL) -> Void = notImplementedSync(),
         promptFor: @escaping (String, Any, [Any]) -> Observable<Any> = notImplemented()) {
         _openURL = openURL
         _promptFor = promptFor
